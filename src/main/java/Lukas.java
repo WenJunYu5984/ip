dc827314d47a1abbc92cfedd1ca746c0851d2896
Wen Jun Yu <e1406517@u.nus.edu>
2026-01-19 18:07:07 +0800

    Renamed file from Duke to Lukas with level 0 implementation

---
+++ src/main/java/Lukas.java
@@ -0,0 +1,11 @@
+public class Lukas {
+    public static void main(String[] args) {
+        String line = "_________________________________________";
+        System.out.println(line);
+        System.out.println("Hello I'm Lukas\n");
+        System.out.println("What can I do for you?\n");
+        System.out.println(line);
+        System.out.println("Bye! Hope to see you again soon!\n");
+        System.out.println(line);
+    }
+}
