0be14aba4367eff7fbc73cd697bc91b34574af11
Wen Jun Yu <e1406517@u.nus.edu>
2026-02-14 21:49:27 +0800

    Refactored spaces

--- src/main/java/lukas/ui/Ui.java
+++ src/main/java/lukas/ui/Ui.java
@@ -3,7 +3,11 @@ package lukas.ui;
 import lukas.task.Task;
 
 public class Ui {
-    private static final String LINE = "    _________________________________________";
+    private static final String LINE = getSpaces() + "_________________________________________";
+
+    private static String getSpaces() {
+        return "    ";
+    }
 
     public static void showLine() {
         System.out.println(LINE);
@@ -11,26 +15,26 @@ public class Ui {
 
     public static void showWelcome() {
         showLine();
-        System.out.println("    Hello I'm Lukas");
-        System.out.println("    What can I do for you?");
+        System.out.println(getSpaces() + "Hello I'm Lukas");
+        System.out.println(getSpaces()+ "What can I do for you?");
         showLine();
     }
 
     public static void showGoodbye() {
-        System.out.println("    Bye! See you later, alligator!");
+        System.out.println(getSpaces() + "Bye! See you later, alligator!");
         showLine();
     }
 
     public static void showAdded(Task task, int count) {
-        System.out.println("    Got it. I've added this task:");
-        System.out.println("      " + task);
-        System.out.println("    Now you have " + count + " tasks in the list.");
+        System.out.println(getSpaces() + "Got it. I've added this task:");
+        System.out.println(getSpaces() + task);
+        System.out.println(getSpaces() + "Now you have " + count + " tasks in the list.");
     }
 
     public static void showList(Task[] list, int count) {
-        System.out.println("    Here are the tasks in your list:");
+        System.out.println(getSpaces() + "Here are the tasks in your list:");
         for (int i = 0; i < count; i++) {
-            System.out.println("    " + (i + 1) + "." + list[i]);
+            System.out.println(getSpaces() + (i + 1) + "." + list[i]);
         }
     }
 }
