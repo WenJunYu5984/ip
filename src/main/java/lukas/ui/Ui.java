a6e39209ef2abd60d66640c54dc9e9fbd701f0b7
Wen Jun Yu <e1406517@u.nus.edu>
2026-02-14 22:05:41 +0800

    Using A-collections for dynamic allocation of tasks

--- src/main/java/lukas/ui/Ui.java
+++ src/main/java/lukas/ui/Ui.java
@@ -2,12 +2,10 @@ package lukas.ui;
 
 import lukas.task.Task;
 
+import java.util.ArrayList;
+
 public class Ui {
     private static final String LINE = getSpaces() + "_________________________________________";
-
-    private static String getSpaces() {
-        return "    ";
-    }
 
     public static void showLine() {
         System.out.println(LINE);
@@ -31,10 +29,25 @@ public class Ui {
         System.out.println(getSpaces() + "Now you have " + count + " tasks in the list.");
     }
 
-    public static void showList(Task[] list, int count) {
-        System.out.println(getSpaces() + "Here are the tasks in your list:");
-        for (int i = 0; i < count; i++) {
-            System.out.println(getSpaces() + (i + 1) + "." + list[i]);
+//    public static void showList(Task[] list, int count) {
+//        System.out.println(getSpaces() + "Here are the tasks in your list:");
+//        for (int i = 0; i < count; i++) {
+//            System.out.println(getSpaces() + (i + 1) + "." + list[i]);
+//        }
+//    }
+    public static void showList(ArrayList<Task> tasks) {
+        if (tasks.isEmpty()) {
+            System.out.println(getSpaces() + "Your list is currently empty!");
+            return;
         }
+
+        System.out.println(getSpaces() + "Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(getSpaces() + (i + 1) + "." + tasks.get(i));
+        }
+    }
+
+    private static String getSpaces() {
+        return "    ";
     }
 }
