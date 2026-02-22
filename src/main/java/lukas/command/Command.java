f64fa0df383bd2539a6eac47f334916156a02ab9
Wen Jun Yu <e1406517@u.nus.edu>
2026-02-22 15:45:27 +0800

    Added command class. Parent class for other classes

--- src/main/java/lukas/command/Command.java
+++ src/main/java/lukas/command/Command.java
@@ -1,4 +1,14 @@
 package lukas.command;
 
-public class Command {
+import lukas.LukasException;
+import lukas.storage.Storage;
+import lukas.task.TaskList;
+import lukas.ui.Ui;
+
+public abstract class Command {
+    public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws LukasException;
+
+    public boolean isExit() {
+        return false;
+    }
 }
